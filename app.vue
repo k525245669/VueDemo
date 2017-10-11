08b2d1bcafb7672eb27e665721fc11e2d81a15e1
朱坤坤 <hzzhukunkun@corp.netease.com>
2017-10-11 23:01:05 +0800

    init project

---
+++ app.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div>Hello {{name}}</div>
+    <v-title title="Vue组件化"></v-title>
+    <v-button @click="handleClick">点击按钮</v-button>
+    <p>
+      <img src="./images/stom.jpg" style="width: 200px;">
+    </p>
+  </div>
+</template>
+<script>
+  import vTitle from './title.vue';
+  import vButton from './button.vue';
+
+  export default {
+    components: {
+      vTitle,
+      vButton
+    },
+    data () {
+      return {
+        name: "Vue.js"
+      }
+    },
+    methods: {
+      handleClick (e) {
+        console.log(e)
+      }
+    }
+  }
+</script>
+<style scoped>
+  div {
+    color: #f60;
+    font-size: 24px;
+  }
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+const config = {
+  entry: {
+    main: './main'
+  },
+  output: {
+    path: path.join(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            css: ExtractTextPlugin.extract({
+              use: 'css-loader',
+              fallback: 'vue-style-loader'
+            }),
+            pass: ExtractTextPlugin.extract({
+              use: 'sass-loader',
+              fallback: 'vue-style-loader'
+            }),
+          }
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: 'css-loader',
+          fallback: 'style-loader'
+        })
+      },
+      {
+        test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*/,
+        loader: 'url-loader?limit=1024'
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('main.css')
+  ]
+}
+
+
+
+module.exports = config;
